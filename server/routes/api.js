1e581246f1b28151c870d4333bc24a9e9742c0bd
BONNEFF Victor <victor.bonneff@efrei.net>
2020-11-06 11:07:58 +0100

    commit

--- server/routes/api.js
+++ server/routes/api.js
@@ -14,9 +14,36 @@ const client = new Client({
 });
 client.connect();
 
-router.post("/login", function (req, res, next) {
-	res.status(500);
-	res.send({ message: "Not implemented" });
+router.post("/login", async(req, res)=> {
+	const email = req.body.mail
+	const password = req.body.password
+	
+	const profil = await client.query({
+		text: 'SELECT * FROM users WHERE mail =$1',
+		values:[mail]
+	})
+	
+	if (profil.row.length === 0) {
+		res.status(401).json({
+			message:"Cet utilisateur n'existe pas"
+		})
+		return
+	}
+
+	const user= profil.rows[0]
+
+	if (await bcrypt.compare(password, user.password)) {
+		req.session.userId = user.id
+		res.json({
+			id: user.id,
+			email: user.mail
+		})
+	} else {
+		res.status(401).json({
+			message:"Mauvais mot de passe"
+		})
+		return
+	}
 });
 
 router.post("/logout", function (req, res, next) {
