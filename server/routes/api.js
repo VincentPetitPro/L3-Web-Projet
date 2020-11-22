c35a702ad99d7964cbf7f25bf2865af2adc34b55
VincentPetitPro <48692227+VincentPetitPro@users.noreply.github.com>
2020-11-22 17:45:46 +0100

    api change

--- server/routes/api.js
+++ server/routes/api.js
@@ -5,136 +5,133 @@ const { Client } = require("pg");
 const config = require("../../config.json");
 
 const client = new Client({
-
 	user: config.user,
 	host: config.host,
 	password: config.password,
 	database: config.database,
-
 });
 client.connect();
 
-
 //connexion au profil
-router.post("/login", async(req, res)=> {
-	const email = req.body.mail
-	const password = req.body.password
-	
+router.post("/login", async (req, res) => {
+	const email = req.body.mail;
+	const password = req.body.password;
+
 	const profil = await client.query({
-		text: 'SELECT * FROM users WHERE mail =$1',
-		values:[email]
-	})
-	
+		text: "SELECT * FROM users WHERE mail =$1",
+		values: [email],
+	});
+
 	if (profil.row.length === 0) {
 		res.status(401).json({
-			message:"Cet utilisateur n'existe pas"
-		})
-		return
+			message: "Cet utilisateur n'existe pas",
+		});
+		return;
 	}
 
-	const user= profil.rows[0]
+	const user = profil.rows[0];
 
 	if (await bcrypt.compare(password, user.password)) {
-		req.session.userId = user.id
+		req.session.userId = user.id;
 		res.json({
 			id: user.id,
-			email: user.mail
-		})
+			email: user.mail,
+		});
 	} else {
 		res.status(401).json({
-			message:"Mauvais mot de passe"
-		})
-		return
+			message: "Mauvais mot de passe",
+		});
+		return;
 	}
 });
 
 //création du profil
 router.post("/register", async (req, res) => {
-	const email = req.body.mail
-	const password = req.body.password
-	const username = req.body.username
+	const email = req.body.mail;
+	const password = req.body.password;
+	const username = req.body.username;
 
-
-	const profil = await client.query({
-		text: 'SELECT * FROM users WHERE mail =$1',
-		values:[email]
-	})
+	const profil = await client.query("SELECT * FROM users WHERE mail =$1", [email]);
 
 	if (profil.rows.length > 0) {
 		res.status(401).json({
-			message:" utilisateur déjà existant"
-		})
-		return
+			message: " utilisateur déjà existant",
+		});
+		return;
 	}
 
-	const hash = await bcrypt.hash(password, 10)
+	const hash = await bcrypt.hash(password, 10);
 
-	
-	await client.query({
-		text: 'INSERT INTO users(email,password,username) VALUES($1, $2, $3)',
-		values:[email,hash,username]
-	})
-	res.send('ok')
+	await client.query("INSERT INTO users(email,password,username) VALUES($1, $2, $3)", [
+		email,
+		hash,
+		username,
+	]);
+	res.send("ok");
 
 	const user = {
-    	email: email,
-    	username: username
-	}
-	
-	if (actual_user.length > 0)
-	{
-		while (actual_user.length != 0)
-		{
-			actual_user.splice(0, 1)
-    	}
-		actual_user.push(user)
-  	}
-  	else{
-   	 	actual_user.push(user)
-  	}
-  res.status(200).json({message: 'Votre inscription a été pris en compte'})
+		email: email,
+		username: username,
+	};
 
+	if (actual_user.length > 0) {
+		while (actual_user.length != 0) {
+			actual_user.splice(0, 1);
+		}
+		actual_user.push(user);
+	} else {
+		actual_user.push(user);
+	}
+	res.status(200).json({ message: "Votre inscription a été pris en compte" });
 });
 
-router.get('/me', async (req, res) => {
-	if (typeof req.session.userId === 'undefined') {
-		res.status(401).json({ message: 'not connected' })
-		return
-	}
-	
-	const result = await client.query({ text: 'SELECT id, mail FROM users WHERE id=$1', values: [req.session.userId] })
-	
-	res.json(result.row[0])
-});
-
-router.post('/article', async (req, res) => {
-	const name = req.body.name
-	const content = req.body.content
-	const image = req.body.img 
-	const author = req.body.author
-
-	if (typeof name !== 'string' || name === '' ||
-			typeof content !== 'string' || content === '' ||
-			typeof image !== 'string' || content === '' ||
-			typeof author !== 'string' || content === '') {
-		res.status(400).json({ message: 'bad request' })
-		return
+router.get("/me", async (req, res) => {
+	if (typeof req.session.userId === "undefined") {
+		res.status(401).json({ message: "not connected" });
+		return;
 	}
 
 	const result = await client.query({
-		text: 'INSERT INTO articles(name, content, image, author) VALUES ($1, $2, $3, $4) RETURNING *',
-		values: [name, content, image, author]
-	})
-	const id = result.rows[0].id
-	
+		text: "SELECT id, mail FROM users WHERE id=$1",
+		values: [req.session.userId],
+	});
+
+	res.json(result.row[0]);
+});
+
+router.post("/article", async (req, res) => {
+	const name = req.body.name;
+	const content = req.body.content;
+	const image = req.body.img;
+	const author = req.body.author;
+
+	if (
+		typeof name !== "string" ||
+		name === "" ||
+		typeof content !== "string" ||
+		content === "" ||
+		typeof image !== "string" ||
+		content === "" ||
+		typeof author !== "string" ||
+		content === ""
+	) {
+		res.status(400).json({ message: "bad request" });
+		return;
+	}
+
+	const result = await client.query({
+		text: "INSERT INTO articles(name, content, image, author) VALUES ($1, $2, $3, $4) RETURNING *",
+		values: [name, content, image, author],
+	});
+	const id = result.rows[0].id;
+
 	res.json({
 		id: id,
 		name: name,
 		content: content,
 		image: image,
-		author: author
-	})
-	
-})
+		author: author,
+	});
+});
 
-module.exports = router
+module.exports = router;
